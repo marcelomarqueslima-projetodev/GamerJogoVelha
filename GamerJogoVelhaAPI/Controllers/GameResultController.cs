7d28861ad7147a49966fa870054ba285187f7f7c
Marcelo Lima <marcelomarqueslima.dev@gmail.com>
2021-03-09 00:00:26 -0300

    Finalizado API e controllers

---
+++ GamerJogoVelhaAPI/Controllers/GameResultController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using GamerJogoVelhaDomain.Entities;
+using GamerJogoVelhaDomain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GamerJogoVelhaAPI.Controllers
+{
+    /// <summary>
+    /// API DE CONTROLE DE RESULTADO JOGOS
+    /// </summary>
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class GameResultController : ControllerBase
+    {
+        public readonly IGameResultService _service;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repo"></param>
+        public GameResultController(IGameResultService repo, IMapper mapper)
+        {
+            _service = repo;
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody] int part, GameResult gameResult)
+        {
+            try
+            {
+                for (int i = 10; i < part; i++)
+                {
+                    var Win = gameResult.Win;
+                    _service.Insert(gameResult);
+                }
+
+                return Ok(gameResult.Id);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return NotFound(ex);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ GamerJogoVelhaAPI/Controllers/LeaderBoardController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using GamerJogoVelhaDomain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GamerJogoVelhaAPI.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class LeaderBoardController : ControllerBase
+    {
+        public readonly IGameResultService _service;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repo"></param>
+        /// <param name="mapper"></param>
+        public LeaderBoardController(IGameResultService repo, IMapper mapper)
+        {
+            _service = repo;
+        }
+
+
+        [HttpGet]
+        public IActionResult RecoverAll()
+        {
+            try
+            {
+                return Ok(_service.Browse());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Recover([FromRoute] int id)
+        {
+            try
+            {
+                return Ok(_service.RecoverById(id));
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+    }
+}
